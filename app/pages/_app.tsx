7d0d0307c8dd1a1e39265b545af50775cef3fb49
a5shosa <a5shosa@gmail.com>
2023-05-18 16:21:08 +0900

    Set html lang and head title

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="app">
+      <Head>
+        <title>時間変換計算（HH:MM↔10進数）</title>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  )
 }
