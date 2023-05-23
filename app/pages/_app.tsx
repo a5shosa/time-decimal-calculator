320c248aab2c9da408e454f70366861950da447a
a5shosa <a5shosa@gmail.com>
2023-05-23 18:27:34 +0900

    Allow Google Analytics to be configured

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,13 +1,45 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import Head from 'next/head'
+import Script from 'next/script'
+import * as gtag from '../lib/gtag'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouterChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouterChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
+
   return (
     <div className="app">
       <Head>
         <title>時間変換計算（HH:MM↔10進数）</title>
       </Head>
+      <Script
+         strategy="afterInteractive"
+         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+       />
+       <Script
+         id="gtag-init"
+         strategy="afterInteractive"
+         dangerouslySetInnerHTML={{
+           __html: `
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+           gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+           `,
+         }}
+       />
       <Component {...pageProps} />
     </div>
   )
