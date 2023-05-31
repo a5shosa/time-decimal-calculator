2961801bd0fa06949a73e30a6617f3091c31d896
a5shosa <a5shosa@gmail.com>
2023-05-31 22:46:00 +0900

    Ignore render <Script /> section when NEXT_PUBLIC_GADS_CLIENT was not set

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -34,11 +34,11 @@ export default function App({ Component, pageProps }: AppProps) {
          strategy="afterInteractive"
          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
        />
-       <Script
+       { process.env.NEXT_PUBLIC_GADS_CLIENT && <Script
          async
          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GADS_CLIENT}`}
          crossOrigin="anonymous"
-       />
+       />}
        <Script
          id="gtag-init"
          strategy="afterInteractive"
